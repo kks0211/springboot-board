1d6b55e99704bae508f6c1b7bdf60e9e68bcb3dc
Kwan <kks0211@hanmail.net>
2020-01-27 13:50:12 +0900

    스프링부트 게시판 업로드

---
+++ src/main/java/com/springboot/board/controller/RestBoardController.java
@@ -0,0 +1,86 @@
+package com.springboot.board.controller;
+
+import com.springboot.board.dto.BoardDto;
+import com.springboot.board.dto.BoardFileDto;
+import com.springboot.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.apache.commons.io.FileUtils;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.net.URLEncoder;
+import java.util.List;
+
+
+@Controller
+public class RestBoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board")
+    public ModelAndView openBoardList() throws Exception {
+        ModelAndView mv = new ModelAndView("/board/restBoardList");
+        List<BoardDto> list = boardService.selectBoardList();
+        mv.addObject("list", list);
+        return mv;
+    }
+
+    @GetMapping("/board/write")
+    public String openBoardWrite() throws Exception {
+        return "/board/restBoardWrite";
+    }
+
+    @PostMapping("/board/write")
+    public String insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
+        boardService.insertBoard(board, multipartHttpServletRequest);
+        return "redirect:/board";
+    }
+
+    @GetMapping("/board/{boardIdx}")
+    public ModelAndView openBoardDetail(@PathVariable("boardIdx") int boardIdx, ModelMap model) throws Exception {
+        ModelAndView mv = new ModelAndView("/board/restBoardDetail");
+
+        BoardDto board = boardService.selectBoardDetail(boardIdx);
+        mv.addObject("board", board);
+
+        return mv;
+    }
+
+    @PutMapping("/board/{boardIdx}")
+    public String updateBoard(BoardDto board) throws Exception {
+        boardService.updateBoard(board);
+        return "redirect:/board";
+    }
+
+    @DeleteMapping("/board/{boardIdx}")
+    public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception {
+        boardService.deleteBoard(boardIdx);
+        return "redirect:/board";
+    }
+
+    @GetMapping("/board/file")
+    public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception {
+        BoardFileDto boardFile = boardService.selectBoardFileInformation(idx, boardIdx);
+        if (ObjectUtils.isEmpty(boardFile) == false) {
+            String fileName = boardFile.getOriginalFileName();
+
+            byte[] files = FileUtils.readFileToByteArray(new File(boardFile.getStoredFilePath()));
+
+            response.setContentType("application/octet-stream");
+            response.setContentLength(files.length);
+            response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(fileName, "UTF-8") + "\";");
+            response.setHeader("Content-Transfer-Encoding", "binary");
+
+            response.getOutputStream().write(files);
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+        }
+    }
+}
